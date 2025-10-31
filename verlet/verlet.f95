86c6932be490d59f1e478a69cd565fcc80231e13
Nikola <ndonov@proton.me>
2025-10-31 14:50:11 +0100

    Some updates to check for conservation of energy

--- verlet/verlet.f95
+++ verlet/verlet.f95
@@ -7,6 +7,7 @@ PROGRAM verlet
   REAL (KIND = wp) :: m
   REAL (KIND = wp), DIMENSION(:), ALLOCATABLE :: pos_k, pos_kp
   REAL (KIND = wp), DIMENSION(:), ALLOCATABLE :: v_k, v_kp
+  REAL (KIND = wp) :: E_k
   INTEGER :: k, n
 
   ALLOCATE(f_a(3))
@@ -15,19 +16,24 @@ PROGRAM verlet
   ALLOCATE(v_k(3))
   ALLOCATE(v_kp(3))
 
-  tau = 0.2_wp ! /s
-  f_a = [0.0, 0.1, 0.0] ! /kg m s^-2 ; constant force with x, y and z components
+  tau = 0.002_wp ! /s
+  f_a = [0.0_wp, 0.1_wp, 0.0_wp] ! /kg m s^-2 ; constant force with x, y and z components
   m = 1_wp ! /kg ; mass of particle
-  pos_k = [0.0, 0.0, 0.0]
-  n = 10 ! 10 iterations of steps k
+  pos_k = [0.0_wp, 0.0_wp, 0.0_wp]
+  n = 100000 ! 100 iterations of steps k
 
   DO k = 1, n
     pos_kp = pos_k + tau*v_k + (tau**2)*(f_a)/(2.0_wp*m)
     f_ap = f_a
     v_kp = v_k + tau/(2.0_wp*m)*(f_a+f_ap)
 
+    E_k = 0.5_wp*m*(v_k(1)**2 + v_k(2)**2 + v_k(3)**2)
+    
     PRINT *, "The x, y and z positions of the particle at the", k, "-th iteration at time =", tau*k, "are:"
     PRINT *, pos_k
+
+    PRINT *, "The potential, kinetic and total energies of the particle at this step are, respectively:"
+    PRINT *, E_k
 
     pos_k = pos_kp
     v_k = v_kp
