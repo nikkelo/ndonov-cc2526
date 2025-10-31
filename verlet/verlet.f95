06a471cdbcd8e1e4c1a54d7703b2c979d7415b5e
Nikola <ndonov@proton.me>
2025-10-31 15:05:19 +0100

    Forgot to add a DEALLOCATE at the end

--- verlet/verlet.f95
+++ verlet/verlet.f95
@@ -11,6 +11,7 @@ PROGRAM verlet
   INTEGER :: k, n
 
   ALLOCATE(f_a(3))
+  ALLOCATE(f_ap(3))
   ALLOCATE(pos_k(3))
   ALLOCATE(pos_kp(3))
   ALLOCATE(v_k(3))
@@ -21,19 +22,19 @@ PROGRAM verlet
   v_k = [0.0_wp, 0.0_wp, 0.0_wp] ! /m s^-1 ; speed of particle
   m = 1_wp ! /kg ; mass of particle
   pos_k = [0.0_wp, 0.0_wp, 0.0_wp]
-  n = 100000 ! 100 iterations of steps k
+  n = 10000 ! 10000 iterations of steps k
 
   DO k = 1, n
     pos_kp = pos_k + tau*v_k + (tau**2)*(f_a)/(2.0_wp*m)
     f_ap = f_a
     v_kp = v_k + tau/(2.0_wp*m)*(f_a+f_ap)
 
-    E_k = 0.5_wp*m*(v_k(1)**2 + v_k(2)**2 + v_k(3)**2)
+    E_k = 0.5_wp*m*(v_kp(1)**2 + v_kp(2)**2 + v_kp(3)**2)
     
     PRINT *, "The x, y and z positions of the particle at the", k, "-th iteration at time =", tau*k, "are:"
-    PRINT *, pos_k
+    PRINT *, pos_kp
 
-    PRINT *, "The potential, kinetic and total energies of the particle at this step are, respectively:"
+    PRINT *, "The kinetic energy of the particle at this step is:"
     PRINT *, E_k
 
     pos_k = pos_kp
@@ -41,4 +42,5 @@ PROGRAM verlet
 
   ENDDO
 
+  DEALLOCATE(f_a, f_ap, pos_k, pos_kp, v_k, v_kp)
 END PROGRAM verlet
