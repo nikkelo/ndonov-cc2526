a89bfadb9246d5fdf90f3dbba40bc2af0d594a9f
Nikola <ndonov@proton.me>
2025-11-07 19:42:10 +0100

    Updated code to properly calculate convergence and stop when converging

--- verlet/verlet.f95
+++ verlet/verlet.f95
@@ -5,7 +5,7 @@ PROGRAM verlet
   REAL (KIND = wp) :: tau, target_time, pos_diff
   REAL (KIND = wp), DIMENSION(:), ALLOCATABLE :: f_a, f_ap
   REAL (KIND = wp) :: m
-  REAL (KIND = wp), DIMENSION(:), ALLOCATABLE :: pos_k, pos_kp
+  REAL (KIND = wp), DIMENSION(:), ALLOCATABLE :: pos_k, pos_kp, pos_temp
   REAL (KIND = wp), DIMENSION(:), ALLOCATABLE :: v_k, v_kp
   REAL (KIND = wp) :: E_k, E_p, E, kinetic_energy
   INTEGER :: k, n
@@ -15,33 +15,38 @@ PROGRAM verlet
   ALLOCATE(f_ap(3))
   ALLOCATE(pos_k(3))
   ALLOCATE(pos_kp(3))
+  ALLOCATE(pos_temp(3))
   ALLOCATE(v_k(3))
   ALLOCATE(v_kp(3))
 
-  tau = 0.02_wp ! /s
+  tau = 2_wp ! /s
+  target_time = 120.0_wp ! Simulating for 2 minute
   f_a = [0.0_wp, 0.1_wp, 0.0_wp] ! /kg m s^-2 ; constant force with x, y and z components
-  v_k = [0.0_wp, 0.0_wp, 0.0_wp] ! /m s^-1 ; speed of particle
   m = 1_wp ! /kg ; mass of particle
-  pos_k = [0.0_wp, 0.0_wp, 0.0_wp]
-  n = 1000 ! 10000 iteriations of steps k
-  target_time = 120_wp ! 120 seconds, two minutes of simulation for the convergence check 
-  convergence = .false. 
+  pos_temp = [0.0_wp, 0.0_wp, 0.0_wp]
+  n = 100 ! 10000 iteriations of steps k
+  convergence = .FALSE. 
    
-  DO WHILE (convergence .eqv. .false.)
+  DO WHILE (.NOT. convergence)
+    pos_k = [0.0_wp, 0.0_wp, 0.0_wp]
+    v_k = [0.0_wp, 0.0_wp, 0.0_wp] ! /m s^-1 ; speed of particle
+    n = INT(target_time / tau)
+
     DO k = 1, n
-      pos_kp = pos_k + tau*v_k + (tau**2)*(f_a)/(2.0_wp*m)
+      ! Compute positions, update force, compute velocities
+      pos_kp = pos_k + tau*v_k + (tau**2.0_wp)*(f_a)/(2.0_wp*m)
       f_ap = f_a
       v_kp = v_k + tau/(2.0_wp*m)*(f_a+f_ap)
-
-      E_k = kinetic_energy(m, v_kp(1), v_kp(2), v_kp(3))
-      E_p = -DOT_PRODUCT(f_ap, pos_kp) ! potential energy calculation
-      E = E_k + E_p
-
 
       PRINT *, "PARTICLE POSITIONS AT ITERATION", k, "AND TIMESTEP", tau*k, ":"
       PRINT *, pos_kp
 
       PRINT *
+
+      ! Compute the kinetic, potential and total energy
+      E_k = kinetic_energy(m, v_kp(1), v_kp(2), v_kp(3))
+      E_p = -DOT_PRODUCT(f_ap, pos_kp) ! potential energy calculation
+      E = E_k + E_p
 
       PRINT *, "KINETIC, POTENTIAL AND TOTAL ENERGY OF THE SYSTEM:"
       PRINT *, E_k, E_p, E
@@ -49,22 +54,27 @@ PROGRAM verlet
       PRINT *
       PRINT *
 
+      ! Compute Euclidean distance from the previous loop
+      pos_diff = SQRT(SUM((pos_kp - pos_temp)**2))
+
+      ! Update the positions and velocities
       pos_k = pos_kp
       v_k = v_kp
 
     ENDDO
 
-    pos_diff = SQRT(SUM((pos_kp-pos_k)**2))
-
-    IF (pos_diff < 0.01_wp) THEN
-      convergence = .true.
+    pos_temp = pos_k
+    
+    IF (pos_diff <= 0.01_wp .AND. tau < 0.2_wp) THEN ! Check if convergence has occured within a difference of 1cm
+      convergence = .TRUE.
       PRINT *, "POSITIONS CONVERGED WITH 1 CM PRECISION AFTER 2 MINUTES, AT TIME STEP", tau
-    ELSE
-      tau = tau/2
+    ELSE ! If not converged, half the timestep tau and redo
+      pos_temp = pos_kp
+      tau = tau / 2.0_wp
     ENDIF
   ENDDO
  
-  DEALLOCATE(f_a, f_ap, pos_k, pos_kp, v_k, v_kp)
+  DEALLOCATE(f_a, f_ap, pos_k, pos_kp, v_k, v_kp, pos_temp)
 END PROGRAM verlet
 
 FUNCTION kinetic_energy(m, x, y, z)
